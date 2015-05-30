92a684ae3ce5bebf95c420644ec7cc4e795866d1
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-05-30 23:51:06 +0200

    [HR] Add first two challenges of code.cpp May 2015

---
+++ hackerrank/template.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int foo(int a) {
+  return a;
+}
+int main() {
+  int num1;
+  cin>>num1;
+  cout << foo(num1);
+  return 0;
+}
